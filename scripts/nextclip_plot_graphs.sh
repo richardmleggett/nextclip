4e626e1e3e363d29cd2e1fc71e90196caa852484
leggettr <richard.leggett@tgac.ac.uk>
2013-08-30 15:52:07 +0100

    Various changes.

--- scripts/nextclip_plot_graphs.sh
+++ scripts/nextclip_plot_graphs.sh
@@ -7,13 +7,18 @@
 #          http://www.tgac.ac.uk/richard-leggett/
 
 # Location of NextClip scripts - change as appropriate
-scriptdir=/data/workarea/leggettr/matepairs/scripts
+scriptdir=/Users/leggettr/Downloads/nextclip-master/scripts
 
-lib=$1
+libdir=$1
+lib=$2
+
+readsdir=${libdir}/reads
+graphsdir=${libdir}/graphs
+analysisdir=${libdir}/analysis
 
 echo "Plotting duplication rate graph"
-inputfile=${lib}/reads/${lib}_duplicates.txt
-outputfile=${lib}/graphs/${lib}_duplicates.pdf
+inputfile=${readsdir}/${lib}_duplicates.txt
+outputfile=${graphsdir}/${lib}_duplicates.pdf
 
 if [ -f ${inputfile} ] ; then
     Rscript ${scriptdir}/nextclip_plot_duplication.R ${inputfile} ${outputfile}
@@ -24,8 +29,8 @@ fi
 for read in 1 2
 do
     echo "Plotting GC content for read ${read}"
-    inputfile=${lib}/reads/${lib}_R${read}_gc.txt
-    outputfile=${lib}/graphs/${lib}_R${read}_gc.pdf
+    inputfile=${readsdir}/${lib}_R${read}_gc.txt
+    outputfile=${graphsdir}/${lib}_R${read}_gc.pdf
 
     if [ -f ${inputfile} ] ; then
         Rscript ${scriptdir}/nextclip_plot_gc.R ${inputfile} ${outputfile}
@@ -37,8 +42,8 @@ done
 for type in A B C D
 do
     echo "Plotting cumulative pair lengths for category ${type}"
-    inputfile=${lib}/reads/${lib}_${type}_pair_hist.txt
-    outputfile=${lib}/graphs/${lib}_cumulative_pairs_${type}.pdf
+    inputfile=${readsdir}/${lib}_${type}_pair_hist.txt
+    outputfile=${graphsdir}/${lib}_cumulative_pairs_${type}.pdf
 
     if [ -f ${inputfile} ] ; then
         Rscript ${scriptdir}/nextclip_plot_pair_lengths.R ${inputfile} ${outputfile}
@@ -49,8 +54,8 @@ do
     for read in 1 2
     do
         echo "Plotting lengths for category ${type} read ${read}"
-        inputfile=${lib}/reads/${lib}_${type}_R${read}_hist.txt
-        outputfile=${lib}/graphs/${lib}_lengths_${type}_R${read}.pdf
+        inputfile=${readsdir}/${lib}_${type}_R${read}_hist.txt
+        outputfile=${graphsdir}/${lib}_lengths_${type}_R${read}.pdf
 
         if [ -f ${inputfile} ] ; then
             Rscript ${scriptdir}/nextclip_plot_lengths.R ${inputfile} ${outputfile}
@@ -62,8 +67,8 @@ do
     for kind in mp pe tandem
     do
         echo "Plotting insert size lengths for category ${type} kind ${kind}"
-        inputfile=${lib}/analysis/${lib}_${type}_${kind}.txt
-        outputfile=${lib}/graphs/${lib}_${kind}_${type}.pdf
+        inputfile=${analysisdir}/${lib}_${type}_${kind}.txt
+        outputfile=${graphsdir}/${lib}_${kind}_${type}.pdf
 
         if [ -f ${inputfile} ] ; then
             Rscript ${scriptdir}/nextclip_plot_inserts.R ${inputfile} ${outputfile}
--- scripts/run_lmp.sh
+++ scripts/run_lmp.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 export ref_min_size=0
-export lib=examples/LIB1468
+export libdir=examples/LIB1468
+export lib=LIB1468
 export read_one=LIB1468part_ATCACG_L001_R1_001.fastq
 export read_two=LIB1468part_ATCACG_L001_R2_001.fastq
-export reference=Reference/AL645882.fasta
+export reference=examples/Reference/AL645882.fasta
 export organism="Streptomyces coelicolor"
 scripts/nextclip_lmp_analysis.sh
