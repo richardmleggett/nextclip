eee0b14ff412cd536291200961f3ea9c5ee65d0b
leggettr <richard.leggett@tgac.ac.uk>
2014-08-28 12:09:52 +0100

    Plot duplication only up to 100

--- scripts/nextclip_plot_duplication.R
+++ scripts/nextclip_plot_duplication.R
@@ -12,7 +12,7 @@ output_filename <- args[2]
 pdf(output_filename, width=3, height=2, pointsize=6)
 par(mar = c(3, 3, 1.5, 0.7))
 par(mgp = c(1.8, 0.5, 0))
-input_file = read.table(input_filename, header=TRUE)
+input_file = read.table(input_filename, nrow=100, header=TRUE)
 plot(input_file[,1], input_file[,3], type="o", pch=20, col="red", ann=FALSE, cex=0.75)
 title(xlab="Duplication level")
 title(ylab="% read pairs")
